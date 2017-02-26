8fb2937bad2fcf81499ba6d7c9a1d53669ad4847
Zachary Lentz <zllentz42@gmail.com>
2017-02-25 23:35:27 -0800

    remove config correctly

--- uninstall.sh
+++ uninstall.sh
@@ -4,12 +4,18 @@ source $THIS_DIR/vars.sh
 
 restore_backup() {
   if [ -f $HOME/.$1.$BAK ]; then
-    rm $HOME/.$1
     mv $HOME/.$1.$BAK $HOME/.$1
+  fi
+}
+
+remove_symlink() {
+  if [ -h $HOME/.$1 ]; then
+    rm $HOME/.$1
   fi
 }
 
 for f in $files
 do
+  remove_symlink $f
   restore_backup $f
 done
