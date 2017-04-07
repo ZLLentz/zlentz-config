c2942e0ecccf6cec45e4f16c636d4d8b64314a63
Zachary Lentz <zllentz42@gmail.com>
2017-04-07 15:14:00 -0700

    remove py23 compatibility boilerplate

--- vim/skeleton.py
+++ vim/skeleton.py
@@ -1,10 +1,2 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Module docstring
-"""
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
-from builtins import * # NOQA
-from future import standard library
-standard_libarary.install_aliases()
