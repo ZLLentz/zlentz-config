99520977712fc5629073babe72db2c47858f6465
Zachary Lentz <zllentz42@gmail.com>
2017-06-22 21:02:17 -0700

    make the environment plugin work

--- bash-it/custom.plugins.bash
+++ bash-it/custom.plugins.bash
@@ -3,33 +3,42 @@
 # notably EPICS, which overuses environment variables, and python, which has
 # some broken setups that kill firefox. This is my workaround so I don't
 # waste time debugging PATH bugs.
-envname() {
-  export ENVNAME="$1"
+_set_if_input() {
+  if [ -z "$2" ]; then
+    export "$1"=""
+  else
+    export "$1"="$2"
+  fi
 }
 
-_orig_env_reset() {
-  envname "-"
+set_envname() {
+  _set_if_input "ENVNAME" "$1"
 }
 
-_full_env_reset() {
-  alias reset_env='_orig_env_reset'
-  reset_env
+set_env_reset() {
+  _set_if_input "ENVRESET" "$1"
 }
 
+reset_env() {
+  if [ ! -z "$ENVRESET" ]; then
+    eval "$ENVRESET"
+  fi
+  set_envname
+  set_env_reset
+}
 
 conda_env() {
   reset_env
   export PRE_ENV_PATH="${PATH}"
   pathmunge "${HOME}/conda/bin"
-  envname conda
+  set_envname conda
   if [ ! -z "$1" ]; then
     source activate "$1"
   fi
-  alias reset_env='_conda_env_reset'
+  set_env_reset "_conda_env_reset"
 }
 
 _conda_env_reset() {
   source deactivate
   export PATH="${PRE_ENV_PATH}"
-  _full_env_reset
 }
