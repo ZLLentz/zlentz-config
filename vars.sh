8903a3e4c3ef9d32f83e1ec3b7fdccd60d68dfd2
Zachary Lentz <zllentz42@gmail.com>
2017-06-22 22:36:33 -0700

    add one more line to condarc and include in install/uninstall

--- vars.sh
+++ vars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export BAK="bak.config"
-export files="bashrc bash_profile gitconfig inputrc vimrc vim/skeleton.py"
+export files="bashrc bash_profile gitconfig inputrc condarc vimrc vim/skeleton.py"
 export BASH_IT_DIR="${HOME}/.config/bash-it"
 export VIM_BUNDLE="${HOME}/.vim/bundle"
